d6244757048b923af4cb36b7bd81c4b1f9e21771
Terence Liu <liu_terence@hotmail.com>
2019-03-06 23:38:26 -0800

    Menu Javascript Commit

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("phone").style.top = "0";
+    document.getElementById("laptop").style.top = "0";
+  } else {
+    document.getElementById("phone").style.top = "-3rem";
+    document.getElementById("laptop").style.top = "-5rem";
+
+  }
+
+  prevScrollpos = currentScrollPos;
+}
+//referenced code from https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
