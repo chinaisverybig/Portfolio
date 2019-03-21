fd1619df54eb2635426890acc3574bf17c4ba1c3
Terence Liu <liu_terence@hotmail.com>
2019-03-21 16:11:15 -0700

    Show More Fixed Commit
    Thanks Gary For the Help! #GARY

--- js/show.js
+++ js/show.js
@@ -2,22 +2,29 @@ function showMoreFunction() {
   var dots = document.getElementsByClassName("dots");
   var moreText = document.getElementsByClassName("more");
   var btn = document.getElementsByClassName("showButton");
+  var blocks = document.getElementsByClassName("block");
 
-  for (var i = 0; i < btn.length; i++) {
-    btn[i].onclick = function() {
-      alert("the index position is:" + i + "and the length is:" + btn.length);
-      if (dots[i].style.display === "none") {
-        btn[i].style.transform = "rotate(0deg)";
-        btn[i].style.transitionDuration = "0.35s ease";
-        dots[i].style.display = "inline";
-        moreText[i].style.display = "none";
-      } else {
-        btn[i].style.transform = "rotate(180deg)";
-        btn[i].style.transitionDuration = "0.35s ease";
-        dots[i].style.display = "none";
-        moreText[i].style.display = "inline";
-      }
-    };
+  for (var i = 0; i < blocks.length; i++) {
+    // console.log('i:', i);
+    let btn = blocks[i].getElementsByClassName('showButton')[0];
+    let dot = blocks[i].getElementsByTagName('p')[0].getElementsByClassName('dots')[0];
+    let moreText = blocks[i].getElementsByTagName('p')[0].getElementsByClassName('more')[0];
+    btn.onclick = function() {
+      more(btn, dot, moreText);
+    }
   }
+}
 
+var more = function(btn, dot, moreText) {
+  if (dot.style.display === "none") {
+    btn.style.transform = "rotate(0deg)";
+    btn.style.transitionDuration = "0.35s ease";
+    dot.style.display = "inline";
+    moreText.style.display = "none";
+  } else {
+    btn.style.transform = "rotate(180deg)";
+    btn.style.transitionDuration = "0.35s ease";
+    dot.style.display = "none";
+    moreText.style.display = "inline";
+  }
 }
