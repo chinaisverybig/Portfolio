f6597f7f1571d16481c824ba45b5d2a422c20c32
Terence Liu <liu_terence@hotmail.com>
2019-04-03 12:42:44 -0700

    Fixed On Scroll Menu Bar Appear and Disappear for Safari Browser

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,17 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("phone").style.top = "0";
-    document.getElementById("laptop").style.top = "0";
-  } else {
-    document.getElementById("phone").style.top = "-3rem";
-    document.getElementById("laptop").style.top = "-5rem";
+  if (window.pageYOffset > 20) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("phone").style.top = "0";
+      document.getElementById("laptop").style.top = "0";
+    } else {
+      document.getElementById("phone").style.top = "-3rem";
+      document.getElementById("laptop").style.top = "-5rem";
 
+    }
+
+    prevScrollpos = currentScrollPos;
   }
-
-  prevScrollpos = currentScrollPos;
 }
 //referenced code from https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
