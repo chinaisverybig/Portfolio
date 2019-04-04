88f7977e650bf913799f81e7240e9e9dd0d737a8
Terence Liu <liu_terence@hotmail.com>
2019-04-03 23:43:26 -0700

    Back To Top Button Fix Commit

--- js/goUp.js
+++ js/goUp.js
@@ -1,3 +1,27 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+    document.getElementById("goUpButton").style.bottom = "10rem";
+  } else {
+    document.getElementById("goUpButton").style.bottom = "2rem";
+  }
+  var currentScrollPos = window.pageYOffset;
+  if (window.pageYOffset > 20) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("phone").style.top = "0";
+      document.getElementById("laptop").style.top = "0";
+    } else {
+      document.getElementById("phone").style.top = "-3rem";
+      document.getElementById("laptop").style.top = "-5rem";
+
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+  scrollFunction()
+
+}
+
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     document.getElementById("goUpButton").style.display = "block";
--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,5 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-    document.getElementById("goUpButton").style.bottom = "10rem";
-  } else {
-    document.getElementById("goUpButton").style.bottom = "2rem";
-  }
   var currentScrollPos = window.pageYOffset;
   if (window.pageYOffset > 20) {
     if (prevScrollpos > currentScrollPos) {
@@ -18,7 +13,6 @@ window.onscroll = function() {
 
     prevScrollpos = currentScrollPos;
   }
-  scrollFunction()
 
 }
 //referenced code from https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
